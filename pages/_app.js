e64b707945120d61bde091c78748ccc7ee685082
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-23 16:56:27 +0700

    feat: add layouts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import "../styles/globals.css";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  return (
+    <>
+      <Head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+        <title>Labco</title>
+      </Head>
+      <div className="h-screen flex flex-col items-stretch">
+        <Header
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <div className="h-full flex">
+          <Sidebar
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+          <div className="w-full p-4 flex flex-col items-stretch gap-4">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
