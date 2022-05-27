c7bdaa7248c2a3aff9e96051db3144d91629b30b
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-27 15:27:41 +0700

    feat: add home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
-import Agenda from "../components/Agenda";
-import Welcome from "../components/Welcome";
+import Agenda from "../components/AgendaWidget";
+import Button from "../components/Button";
+import News from "../components/NewsWidget";
+import PageLayout from "../components/PageLayout";
 
-export default function Home() {
+function LandingPage() {
   return (
-    <>
-      <Welcome />
-      <Agenda />
-    </>
+    <PageLayout
+      isAdminPage={false}
+      pageTitle={"LABCO - Home"}
+      menuItems={[
+        { title: "Akun", href: "/akun", icon: "bx bx-user" },
+        { title: "Jadwal Agenda", href: "/agenda", icon: "bx bx-calendar" },
+        { title: "Berita dan Informasi", href: "/berita", icon: "bx bx-news" },
+      ]}
+    >
+      <section className="py-16 px-4 flex flex-col items-center justify-center gap-4 text-center">
+        <p className="text-sm">Senin, 23 Mei 2022, pukul 16.59</p>
+        <h1 className="text-3xl font-bold">Selamat Datang di LABCO</h1>
+        <p className="text-sm">Sistem Informasi Lab</p>
+        <div className="my-6 flex md:flex-col gap-2">
+          <Button.Primary>Masuk</Button.Primary>
+          <Button.Secondary>Buat Akun</Button.Secondary>
+        </div>
+      </section>
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-4 items-start">
+        <Agenda />
+        <News />
+      </div>
+    </PageLayout>
   );
 }
+export default LandingPage;
