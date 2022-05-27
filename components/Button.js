40e7de54379a1d76268c85e846589e5b1017decd
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-27 15:22:27 +0700

    feat: add `Button` component

---
+++ components/Button.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class Button extends Component {
+  static Primary = ({ children, ...props }) => (
+    <button
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+
+  static Secondary = ({ children, ...props }) => (
+    <button
+      className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+
+  render() {
+    return <></>;
+  }
+}
+
+export default Button;
