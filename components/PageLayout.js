9dde7377bc607597d338e21b9f096bed30c03045
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-30 20:07:40 +0700

    feat: remove menuItems props

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -3,8 +3,93 @@ import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { useState } from "react";
 
-function PageLayout({ pageTitle, menuItems, children, isAdminPage }) {
+function PageLayout({ pageTitle, children, isAdminPage }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const menuItems = {
+    admin: [
+      {
+        title: "Dashboard",
+        href: "/admin",
+        icon: "bx bx-home",
+        activeIcon: "bx bxs-home",
+      },
+      {
+        title: "Manajemen Inventaris",
+        href: "/admin/inventaris",
+        icon: "bx bx-package",
+        activeIcon: "bx bxs-package",
+      },
+      {
+        title: "Manajemen User",
+        href: "/admin/user",
+        icon: "bx bx-user",
+        activeIcon: "bx bxs-user",
+      },
+      {
+        title: "Manajemen Konten Informasi dan Berita",
+        href: "/admin/konten",
+        icon: "bx bx-news",
+        activeIcon: "bx bxs-news",
+      },
+      {
+        title: "Manajemen Agenda",
+        href: "/admin/agenda",
+        icon: "bx bx-calendar",
+        activeIcon: "bx bxs-calendar",
+      },
+      {
+        title: (
+          <>
+            <div className="text-red-600">Logout</div>
+          </>
+        ),
+        href: "/logout",
+        icon: "bx bx-exit",
+      },
+    ],
+    user: [
+      {
+        title: "Home",
+        href: "/",
+        icon: "bx bx-home",
+        activeIcon: "bx bxs-home",
+      },
+      {
+        title: "Akun",
+        href: "/akun",
+        icon: "bx bx-user",
+        activeIcon: "bx bxs-user",
+      },
+      {
+        title: "Isi Buku Tamu",
+        href: "/buku-tamu",
+        icon: "bx bx-book",
+        activeIcon: "bx bxs-book",
+      },
+      {
+        title: "Jadwal Agenda",
+        href: "/agenda",
+        icon: "bx bx-calendar",
+        activeIcon: "bx bxs-calendar",
+      },
+      {
+        title: "Berita dan Informasi",
+        href: "/berita",
+        icon: "bx bx-news",
+        activeIcon: "bx bxs-news",
+      },
+      {
+        title: (
+          <>
+            <div className="text-red-600">Logout</div>
+          </>
+        ),
+        href: "/logout",
+        icon: "bx bx-exit",
+      },
+    ],
+  };
+
   return (
     <>
       <Head>
@@ -24,7 +109,7 @@ function PageLayout({ pageTitle, menuItems, children, isAdminPage }) {
           <Sidebar
             isSidebarOpen={isSidebarOpen}
             setIsSidebarOpen={setIsSidebarOpen}
-            menuItems={menuItems}
+            menuItems={isAdminPage ? menuItems.admin : menuItems.user}
           />
           <div className="w-full h-auto flex-auto p-4 flex flex-col items-stretch gap-4">
             {children}
