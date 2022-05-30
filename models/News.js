57984134ba23d9026ce790f6535a00307b1c42d9
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-30 08:46:53 +0700

    feat: add News schema model

---
+++ models/News.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const NewsSchema = new mongoose.Schema({
+  slug: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  coverImage: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.News || mongoose.model("News", NewsSchema);
