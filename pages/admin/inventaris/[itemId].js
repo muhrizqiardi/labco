ee3498e34183e71a1b33ad9f5d62772b3e2035c0
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-05-31 08:01:56 +0700

    remove unsed line

--- pages/admin/inventaris/[itemId].js
+++ pages/admin/inventaris/[itemId].js
@@ -8,41 +8,11 @@ function ItemDetail() {
   const [fileSrc, setFileSrc] = useState();
   const [name, setName] = useState("");
 
-  // create form submit handler, send post request to /api/items, include fileSrc, name, qty, and condition
-
   return (
     <>
       <PageLayout
         isAdminPage={true}
         pageTitle={"LABCO Admin - Barang Baru"}
-        menuItems={[
-          {
-            title: "Dashboard",
-            href: "/admin",
-            icon: "bx bx-home",
-          },
-          {
-            title: "Manajemen Inventaris",
-            href: "/admin/inventaris",
-            icon: "bx bxs-user",
-            isCurrentPage: true,
-          },
-          { title: "Manajemen User", href: "/admin/user", icon: "bx bx-user" },
-          {
-            title: "Manajemen Konten Informasi dan Berita",
-            href: "/admin/konten",
-            icon: "bx bx-news",
-          },
-          {
-            title: (
-              <>
-                <div className="text-red-600">Logout</div>
-              </>
-            ),
-            href: "/logout",
-            icon: "bx bx-exit",
-          },
-        ]}
       >
         <h1 className="text-2xl font-bold">Edit Barang</h1>
         <form
@@ -64,7 +34,7 @@ function ItemDetail() {
               <select
                 required
                 name="condition"
-                class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none   focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none   focus:outline-none focus:ring-0 focus:border-gray-200 peer"
               >
                 <option selected>Pilih kondisi</option>
                 <option value="true">Baik</option>
