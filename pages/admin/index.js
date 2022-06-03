af73332197856adb2431998fb33818b68dd635d1
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-06-03 07:37:03 +0700

    feat: add auth to admin dashboard

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,43 +2,19 @@ import Head from "next/head";
 import Agenda from "../../components/AgendaWidget";
 import PageLayout from "../../components/PageLayout";
 import Welcome from "../../components/Welcome";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 // Admin Dashboard
 export default function AdminDashboard() {
-  return (
+  const { data: session } = useSession();
+  return session ? (
     <>
       <PageLayout
         isAdminPage={true}
         pageTitle={"LABCO Admin - Home"}
-        menuItems={[
-          {
-            title: "Dashboard",
-            href: "/admin",
-            icon: "bx bxs-home",
-            isCurrentPage: true,
-          },
-          {
-            title: "Manajemen Inventaris",
-            href: "/admin/inventaris",
-            icon: "bx bx-user",
-          },
-          { title: "Manajemen User", href: "/admin/user", icon: "bx bx-user" },
-          {
-            title: "Manajemen Konten Informasi dan Berita",
-            href: "/admin/konten",
-            icon: "bx bx-news",
-          },
-          {
-            title: (
-              <>
-                <div className="text-red-600">Logout</div>
-              </>
-            ),
-            href: "/logout",
-            icon: "bx bx-exit",
-          },
-        ]}
       ></PageLayout>
     </>
+  ) : (
+    <>Anda tidak diperbolehkan memasuki halaman ini</>
   );
 }
