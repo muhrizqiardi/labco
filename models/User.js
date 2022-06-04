f2a4c14fe639bf5bd2ee53527745a03a62bd7da7
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-06-04 20:37:26 +0700

    remove password

--- models/User.js
+++ models/User.js
@@ -9,17 +9,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
   role: {
     type: String,
     required: true,
   },
   profilePicture: {
     type: String,
-    required: true,
   },
 });
 
