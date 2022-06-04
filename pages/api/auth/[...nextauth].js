cdef3939300b15cd7d96b2a11c1c21f3abc7064d
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-06-04 20:40:24 +0700

    feat: add sign in callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import dbConnect from "../../../lib/dbConnect";
+import User from "../../../models/User";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,34 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    signIn: async ({ user }) => {
+      let userExists;
+
+      await dbConnect();
+
+      // Find user by email
+      try {
+        await User.findOne({ email: user.email });
+      } catch (error) {
+        userExists = false;
+      }
+
+      // If user does not exist, create one
+      if (!userExists) {
+        let newUser;
+        try {
+          newUser = await User.create({
+            email: user.email,
+            fullName: user.name,
+            role: "user",
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      return true;
+    },
+  },
 });
