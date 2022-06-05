7f81ea87e1c6ac9a9b373f4b428c3a2ff16d5361
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-06-05 16:08:54 +0700

    feat: update model to include timestamps

--- models/Item.js
+++ models/Item.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
-const ItemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const ItemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    condition: {
+      type: Boolean,
+      required: true,
+    },
+    image: {
+      type: Boolean,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  condition: {
-    type: Boolean,
-    required: true,
-  },
-  image: {
-    type: Boolean,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.Item || mongoose.model("Item", ItemSchema);
--- models/User.js
+++ models/User.js
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
